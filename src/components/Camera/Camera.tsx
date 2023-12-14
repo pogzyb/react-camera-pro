64fa240abef52e94678c7095a6a4e0e1014903f8
pogzyb <pogzyb@umich.edu>
2023-12-14 16:07:14 -0500

    Update Camera.tsx

--- src/components/Camera/Camera.tsx
+++ src/components/Camera/Camera.tsx
@@ -97,6 +97,9 @@ export const Camera = React.forwardRef<unknown, CameraProps>(
       getNumberOfCameras: () => {
         return numberOfCameras;
       },
+      getStream: () => {
+        return stream;
+      },
     }));
 
     useEffect(() => {
